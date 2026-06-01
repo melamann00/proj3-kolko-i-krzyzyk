6dbe30514b5715dd4c1f3778675500330c4ba939
Jan Nowak <jn816813@gmail.com>
2026-06-01 22:03:05 +0200

    Create cmake_clean.cmake

---
+++ build/CMakeFiles/kolko_krzyzyk.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kolko_krzyzyk.dir/link.d"
+  "CMakeFiles/kolko_krzyzyk.dir/proj3.cpp.o"
+  "CMakeFiles/kolko_krzyzyk.dir/proj3.cpp.o.d"
+  "kolko_krzyzyk"
+  "kolko_krzyzyk.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kolko_krzyzyk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
